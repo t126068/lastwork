f434c84fe837756a88392d865ede5aa5b360bff7
t126068 <287565829+t126068@users.noreply.github.com>
2026-07-13 10:20:26 +0900

    refactor: Reorg directory structure and split components

--- js/script.js
+++ js/script.js
@@ -1,38 +1,5 @@
-const placeList = [
-  {
-    name: '高島城',
-    img: 'images/bl-023-1.webp',
-    url: 'place/takashimajou/',
-  },
-  {
-    name: '諏訪大社下社秋宮',
-    img: 'images/bl-035-1.webp',
-    url: 'place/suwa-shrine/',
-  },
-  {
-    name: '諏訪湖',
-    img: 'images/34623407_m.webp',
-    url: 'place/suwalake/',
-  },
-  {
-    name: 'SUWAガラスの里',
-    img: 'images/bl-027-1.webp',
-    url: 'place/suwa-glass/',
-  },
-]
-
-const eventList = [
-  {
-    name: '茅野どんばん',
-    img: 'images/noimage.webp',
-    url: 'place/tino-donban/',
-  },
-  {
-    name: '諏訪湖祭湖上花火大会',
-    img: 'images/hnb0034-049.webp',
-    url: 'events/suwa-lake-hanabi/',
-  },
-]
+import placeList from "../lib/data/placeList.js"
+import eventList from "../lib/data/eventList.js"
 
 const places = document.querySelector('#places')
 const events = document.querySelector('#events')
