0b011d1ad09cdbdc01a395a8d0cc50b30c439830
t126068 <287565829+t126068@users.noreply.github.com>
2026-07-17 10:00:53 +0900

    feat: Change a number of place list items from 4 to 2

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import eventList from "../lib/data/eventList.js"
 const places = document.querySelector('#places')
 const events = document.querySelector('#events')
 
-for(let i = 0; i < 4; i++) {
+for(let i = 0; i < 2; i++) {
   const {name, img, url} = placeList[i]
   const content = `<a class="relative overflow-hidden rounded-md aspect-square" href="${url}"><img src="${img}" class="object-cover w-full h-full"/><div class="absolute bottom-0 w-full h-12 bg-[#00000040] z-1"></div> <p class="absolute m-2 bottom-0 text-white text-lg font-bold z-2">${name}</p></a>`
   places.insertAdjacentHTML('beforeend', content)
