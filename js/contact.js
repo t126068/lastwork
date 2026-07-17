4bfbfc9ffc0132e98285b337f01181806e134a82
t126068 <287565829+t126068@users.noreply.github.com>
2026-07-17 13:51:42 +0900

    feat: Add contact page

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#contact')
+const notice = document.querySelector('#notice')
+
+form.addEventListener('submit', (e) => {
+  const reqs = form.querySelectorAll('.must input, .must textarea')
+  let hasEmpty = false
+
+  reqs.forEach(req => {
+    const message = req.closest('label').querySelector('p')
+    if(req.value.trim() === '') {
+      hasEmpty = true
+      message.textContent = '入力してください'
+    } else {
+      message.textContent = ''
+    }
+  })
+
+  if(hasEmpty) {
+    e.preventDefault()
+    notice.textContent = '入力内容に問題があります。確認して再度お試しください。'
+    notice.className = 'm-2 p-1 border border-black'
+  } else {
+    notice.textContent = ''
+    notice.className = ''
+  }
+})
